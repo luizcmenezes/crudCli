79b4409faef25a6a996709d43410ff7de68c48ee
Luiz <lcmscontas@gmail.com>
2022-02-02 21:41:07 -0300

    Created project

---
+++ backend/src/main/java/com/luiz/crudCli/CrudCliApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.crudCli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCliApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/luiz/crudCli/CrudCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.crudCli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
